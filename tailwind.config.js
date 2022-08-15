4e2b58d8df3f1870b3fa861021f20b09da3c5e74
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-08-15 10:43:59 -0400

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         custom: {
           background: 'var(--background)',
           'btn-submit': '#3b5dd6',
+          'bg-off-light': '#f6f8fb',
+          'bg-off-dark': '#f6f8fb',
         },
       },
     },
