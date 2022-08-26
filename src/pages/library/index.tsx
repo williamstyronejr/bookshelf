68b55ce1b944b8040b850856dda7bb70236410cb
williamstyronejr <itenory@gmail.com>
2022-08-26 09:10:02 -0400

    Moved search to all pages

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,31 +1,5 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
-
-const SearchBar = () => {
-  const [search, setSearch] = useState('');
-  const router = useRouter();
-
-  return (
-    <div className="relative">
-      <i className="focus-within:text-black absolute top-3.5 left-2 text-slate-400 fas fa-search " />
-
-      <input
-        className="bg-white py-2 pr-4 pl-8 border rounded-lg border-slate-500 focus:shadow-[0_0_0_1px_rgba(59,93,214,1)]"
-        name="search"
-        type="text"
-        placeholder="Search by title, author, tags, etc"
-        value={search}
-        onChange={(evt) => setSearch(evt.target.value)}
-        onKeyDown={(evt) => {
-          if (evt.key === 'Enter' && search !== '')
-            router.push(`/library/search?q=${search}`);
-        }}
-      />
-    </div>
-  );
-};
 
 export const getServerSideProps = () => {
   return {
@@ -75,10 +49,6 @@ export const getServerSideProps = () => {
 export default function LibraryPage({ topBooks }) {
   return (
     <section>
-      <header className="mb-8">
-        <SearchBar />
-      </header>
-
       <div className="">
         <ul className="flex flex-row flex-nowrap w-full">
           {topBooks.map((book) => (
