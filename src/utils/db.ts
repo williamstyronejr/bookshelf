d87464c9270750e4310ca443a9887433308bd528
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-08-31 23:03:21 -0400

    Moved away from BigInt

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,12 +5,6 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// @ts-ignore: Unreachable code error
-// eslint-disable-next-line
-BigInt.prototype.toJSON = function (): string {
-  return this.toString();
-};
-
 export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
