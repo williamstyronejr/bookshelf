c85e06b80ec80322e2363cce193cd3707241ab45
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-08-31 23:02:05 -0400

    Initital commit

---
+++ src/pages/api/language/create.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../utils/db';
+
+type Data = {
+  id: number;
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | string>
+) {
+  const { method, body } = req;
+
+  if (method !== 'POST') return res.status(404);
+
+  try {
+    const language = await prisma.bookLanguage.create({
+      data: {
+        name: body.name,
+      },
+      select: { id: true, name: true },
+    });
+
+    res.status(200).json(language);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('');
+  }
+}
