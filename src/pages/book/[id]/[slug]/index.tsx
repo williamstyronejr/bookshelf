76e799fddb425b3e24f369dcc0111f6fd24d3eaf
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-08-31 23:07:46 -0400

    Now fetches book data

--- src/pages/book/[id]/[slug]/index.tsx
+++ src/pages/book/[id]/[slug]/index.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
+import { prisma } from '../../../../utils/db';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   if (!ctx.params || !ctx.params.slug)
@@ -7,35 +9,93 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       notFound: true,
     };
 
-  const test = {
-    id: 'test',
-    title: 'Title',
-    author: 'Author',
-    displayImage: 'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-  };
+  let book = await prisma.book.findUnique({
+    where: {
+      id: parseInt(ctx.params.id?.toString() || ''),
+    },
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+        },
+      },
+      publisher: true,
+      Favorite: true,
+      Review: true,
+      BookGenres: {
+        include: {
+          genre: true,
+        },
+      },
+    },
+  });
+
+  book = JSON.parse(JSON.stringify(book));
+  if (!book) return { notFound: true };
+
+  if (book.slug !== ctx.params.slug)
+    return {
+      redirect: { destination: `/book/${book.id}/${book.slug}` },
+      props: { book },
+    };
 
   return {
-    props: { book: test },
+    props: { book },
   };
 };
 
 export default function BookPage({ book }) {
+  console.log(book);
   return (
     <section className="">
-      <aside className="relative">
-        <Image
-          className="rounded-lg"
-          priority={true}
-          layout="fill"
-          // objectFit="contain"
-          src={book.displayImage}
-          alt="Book covers"
-        />
-      </aside>
+      <header className="flex flex-row flex-nowrap">
+        <div className="relative w-20 h-20">
+          <Image
+            className="rounded-lg"
+            priority={true}
+            layout="fill"
+            // objectFit="contain"
+            src={book.displayImage}
+            alt="Book covers"
+          />
+        </div>
 
-      <div>
-        <h4>Reviews</h4>
-      </div>
+        <div>
+          <h3>{book.title}</h3>
+
+          <div>
+            <Link href={`/author/${book.author.id}/${book.author.slug}`}>
+              <a>By {book.author.name}</a>
+            </Link>
+          </div>
+
+          <div>
+            <h4>Description</h4>
+
+            <div>{book.description}</div>
+          </div>
+
+          <div>
+            <h4>Details</h4>
+
+            <div>{book.publisher.name}</div>
+            <div>{book.isbn13}</div>
+            <div>{book.publishedDate}</div>
+            <div>{book.pageCount} Pages</div>
+          </div>
+
+          <div>
+            <div>{book.copiesCount} Book Left</div>
+            <Link href={`/books/${book.id}/${book.slug}/reserve`}>
+              <a>Reserve Today!</a>
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      <div>Reviews</div>
     </section>
   );
 }
