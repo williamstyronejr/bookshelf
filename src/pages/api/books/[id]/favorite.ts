0f459b919e7f22edd0f60d74d03080dc09e5c9bd
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-02 00:42:40 -0400

    Initial commit

---
+++ src/pages/api/books/[id]/favorite.ts
@@ -0,0 +1,76 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerAuthSession } from '../../../../utils/serverSession';
+import { prisma } from '../../../../utils/db';
+import { Favorite } from '@prisma/client';
+
+type Data = {
+  favorite: boolean;
+  data: Favorite | null;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  const session = await getServerAuthSession({ req, res });
+  if (!id) return res.status(400).end();
+  if (!session || !session.user || !session.user.id)
+    return res.status(401).redirect('/api/auth/signin');
+
+  const bookId = parseInt(id.toString());
+
+  switch (method) {
+    case 'GET': {
+      const favorite = await prisma.favorite.findUnique({
+        where: {
+          userId_bookId: {
+            userId: session.user.id,
+            bookId: bookId,
+          },
+        },
+      });
+
+      return res.status(200).json({ favorite: !!favorite, data: favorite });
+    }
+
+    case 'POST': {
+      let initialFavorite = await prisma.favorite.findUnique({
+        where: {
+          userId_bookId: {
+            userId: session.user.id,
+            bookId: bookId,
+          },
+        },
+      });
+
+      if (initialFavorite) {
+        await prisma.favorite.delete({ where: { id: initialFavorite.id } });
+        return res.status(200).json({ favorite: false, data: null });
+      } else {
+        const favorite = await prisma.favorite.create({
+          data: {
+            book: {
+              connect: {
+                id: bookId,
+              },
+            },
+            user: {
+              connect: {
+                id: session.user.id,
+              },
+            },
+          },
+        });
+        return res.status(200).json({ favorite: true, data: favorite });
+      }
+    }
+
+    default:
+      return res.status(404).end();
+  }
+}
