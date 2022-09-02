d395f584781caad454b556b07b00c6f2a56df38c
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-02 00:41:53 -0400

    Typing for getserversideprops

--- src/utils/serverSession.ts
+++ src/utils/serverSession.ts
@@ -1,10 +1,16 @@
+import { IncomingMessage, ServerResponse } from 'http';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth/next';
+import { NextApiRequestCookies } from 'next/dist/server/api-utils';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
 
 export async function getServerAuthSession(ctx: {
-  req: NextApiRequest;
-  res: NextApiResponse;
+  req:
+    | NextApiRequest
+    | (IncomingMessage & {
+        cookies: NextApiRequestCookies;
+      });
+  res: NextApiResponse | ServerResponse;
 }) {
   return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
 }
