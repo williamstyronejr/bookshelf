9f21ecf0bef60ba36b134e3ba0be5f062e3784f0
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-02 00:44:00 -0400

    Now fetches data from server

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,83 +1,101 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { prisma } from '../../utils/db';
 
-export const getServerSideProps = () => {
+export const getServerSideProps = async () => {
+  let mostRecentBooks = await prisma.book.findMany({
+    take: 20,
+    orderBy: [
+      {
+        createdAt: 'asc',
+      },
+    ],
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+        },
+      },
+    },
+  });
+
+  let topBooks = await prisma.book.findMany({
+    take: 20,
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+        },
+      },
+    },
+  });
+
+  // Hacky way of dealing with Date Object not having a toJSON
+  mostRecentBooks = JSON.parse(JSON.stringify(mostRecentBooks));
+  topBooks = JSON.parse(JSON.stringify(topBooks));
+
+  console.log(mostRecentBooks);
+
   return {
     props: {
-      topBooks: [
-        {
-          id: 'test',
-          slug: 'test',
-          title: 'Title',
-          author: {
-            name: 'author name',
-            id: 1,
-            slug: 'author slug',
-          },
-          displayImage:
-            'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-        },
-        {
-          id: 'test2',
-          title: 'Title 2',
-          slug: 'test',
-          author: {
-            name: 'author name',
-            id: 1,
-            slug: 'author slug',
-          },
-          displayImage:
-            'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-        },
-        {
-          id: 'test3',
-          title: 'Title 3',
-          slug: 'test',
-          author: {
-            name: 'author name',
-            id: 1,
-            slug: 'author slug',
-          },
-          displayImage:
-            'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-        },
-      ],
+      mostRecentBooks,
+      topBooks,
     },
   };
 };
 
-export default function LibraryPage({ topBooks }) {
+const BookItem = ({ book }) => (
+  <li className="relative flex-grow w-1/5 mr-6 max-w-[175px]">
+    <Link href={`/book/${book.id}/${book.slug}`}>
+      <a>
+        <div className="relative h-56 shadow-md mb-2">
+          <Image
+            className="rounded-lg"
+            priority={true}
+            layout="fill"
+            src={book.displayImage}
+            alt="Book covers"
+          />
+        </div>
+        <div className="font-medium">{book.title}</div>
+      </a>
+    </Link>
+
+    <Link href={`/author/${book.author.id}/${book.author.slug}`}>
+      <a>
+        <div className="text-gray-600">{book.author.name}</div>
+      </a>
+    </Link>
+  </li>
+);
+
+export default function LibraryPage({ topBooks, mostRecentBooks }) {
+  console.log(topBooks);
   return (
     <section>
-      <div className="">
+      <header className="">
+        <h4 className="py-4 text-xl">Most Popular</h4>
         <ul className="flex flex-row flex-nowrap w-full">
-          {topBooks.map((book) => (
-            <li
-              key={book.id}
-              className="relative flex-grow h-56 w-1/5 mr-6 max-w-[175px]"
-            >
-              <Link href={`/book/${book.id}/${book.slug}`}>
-                <a>
-                  <div className="relative h-56 shadow-md mb-2">
-                    <Image
-                      className="rounded-lg"
-                      priority={true}
-                      layout="fill"
-                      src={book.displayImage}
-                      alt="Book covers"
-                    />
-                  </div>
-                  <div className="font-medium">{book.title}</div>
-                </a>
-              </Link>
+          {topBooks
+            ? topBooks.map((book) => (
+                <BookItem key={`popular-${book.id}`} book={book} />
+              ))
+            : null}
+        </ul>
+      </header>
 
-              <Link href={`/author/${book.author.id}/${book.author.slug}`}>
-                <a>
-                  <div className="text-gray-600">{book.author.name}</div>
-                </a>
-              </Link>
-            </li>
-          ))}
+      <div className="block">
+        <h4 className="py-4 text-xl">New Releases</h4>
+        <ul>
+          {mostRecentBooks
+            ? mostRecentBooks.map((book) => (
+                <BookItem key={`recent-${book.id}`} book={book} />
+              ))
+            : null}
         </ul>
       </div>
     </section>
