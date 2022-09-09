5e2692abedca7a2faea100f4de875be7cd61e6ca
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-09 10:56:50 -0400

    Replaced test data with data from DB

--- src/pages/api/books/[id]/reservation.ts
+++ src/pages/api/books/[id]/reservation.ts
@@ -1,36 +1,75 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import crypto from 'crypto';
+import { prisma } from '../../../../utils/db';
+import Redis from 'ioredis';
+import dayjs from 'dayjs';
+
+import { getServerAuthSession } from '../../../../utils/serverSession';
+
+const redisClient = new Redis(process.env.REDIS_URL?.toString() || '');
 
 type Data = {
   book?: any;
   reservation?: any;
 };
 
-export default function handler(
+type InputError = {
+  reserveLength?: string;
+  timeout?: boolean;
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | InputError>
 ) {
-  const { body, method } = req;
+  const {
+    query: { id },
+    body: { reserveLength },
+    method,
+  } = req;
+  const session = await getServerAuthSession({ req, res });
 
-  if (method === 'GET') {
-    return res.status(200).json({
-      book: {
-        id: crypto.pseudoRandomBytes(10).toString('hex'),
-        title: 'Title',
-        author: 'Author',
-        displayImage:
-          'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
+  if (!session || !session.user || !session.user.id)
+    return res.status(401).end();
+  if (method !== 'POST') return res.status(404).end();
+  if (!reserveLength || !id)
+    return res
+      .status(400)
+      .json({ reserveLength: 'Length of reservation is required.' });
+
+  try {
+    const holdExists = await redisClient.zscore(
+      `reserve-${id}`,
+      session.user.id.toString()
+    );
+
+    if (!holdExists) {
+      return res.status(400).json({
+        timeout: true,
+      });
+    }
+
+    const reservation = await prisma.reservation.create({
+      data: {
+        book: {
+          connect: {
+            id: parseInt(id.toString()),
+          },
+        },
+        user: {
+          connect: {
+            id: session.user.id,
+          },
+        },
+        dueDate: dayjs(Date.now()).add(reserveLength, 'days').toDate(),
       },
     });
-  } else if (method === 'POST') {
-    // Save reservation
-    const { reserveLength } = JSON.parse(body);
-    console.log(reserveLength);
 
+    await redisClient.zrem(`reserve-${id}`, session.user.id);
     return res.status(200).json({
-      reservation: {
-        id: '1234',
-      },
+      reservation: JSON.parse(JSON.stringify(reservation)),
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).end();
   }
 }
