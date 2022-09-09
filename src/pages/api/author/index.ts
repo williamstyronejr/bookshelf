9f588c200c70b6202ce591353885bd5330372385
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-09 10:55:17 -0400

    Replaced test data with real fetch from DB

--- src/pages/api/author/index.ts
+++ src/pages/api/author/index.ts
@@ -1,43 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import crypto from 'crypto';
+import { prisma } from '../../../utils/db';
+import { getServerAuthSession } from '../../../utils/serverSession';
+import { validatePagination } from '../../../utils/validation';
 
 type Data = {
   results: Array<any>;
-  nextPage: Number;
+  nextPage: Number | null;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | string>
 ) {
-  const {
-    query: { page },
-    method,
-  } = req;
+  const { query, method } = req;
+  const session = await getServerAuthSession({ req, res });
 
   if (method !== 'GET') return res.status(404).send('');
-  if (!page) return res.status(200).json({ results: [], nextPage: 0 });
+  if (!query.page) return res.status(200).json({ results: [], nextPage: 0 });
 
-  res.status(200).json({
-    nextPage: parseInt(page.toString()) + 1,
-    results: [
-      {
-        id: crypto.pseudoRandomBytes(10).toString('hex'),
-        name: 'Title',
-        slug: 'Author',
-        bio: '',
-        profileImage:
-          'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
+  try {
+    const page = query.page ? parseInt(query.page.toString()) : NaN;
+    const take = query.limit ? parseInt(query.limit.toString()) : 10;
+    if (!validatePagination(page, take))
+      return res.status(200).json({ results: [], nextPage: 0 });
+
+    const authors = await prisma.author.findMany({
+      take,
+      skip: page * take,
+      include: {
+        books: {
+          take: 3,
+        },
       },
-      {
-        id: crypto.pseudoRandomBytes(10).toString('hex'),
-        name: 'Title',
-        slug: 'Author',
-        bio: '',
-        profileImage:
-          'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-      },
-    ],
-  });
+    });
+
+    return res.status(200).json({
+      results: JSON.parse(JSON.stringify(authors)),
+      nextPage: authors.length === take ? page + 1 : null,
+    });
+  } catch (err) {
+    res.status(500);
+  }
 }
