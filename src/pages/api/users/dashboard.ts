9a6dde08af1398837437a9d3f00e8ef44cf04c56
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-09 10:57:01 -0400

    Replaced test data with data from DB

--- src/pages/api/users/dashboard.ts
+++ src/pages/api/users/dashboard.ts
@@ -1,44 +1,78 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Genre, GenreUserCount } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import crypto from 'crypto';
+import { prisma } from '../../../utils/db';
+import { getServerAuthSession } from '../../../utils/serverSession';
 
 type Data = {
   current: Array<any>;
   favoriteAuthor: Array<{ name: string; slug: string }>;
+  favoriteGenres: Array<
+    GenreUserCount & {
+      genre: Genre;
+    }
+  >;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({
-    current: [
-      {
-        id: crypto.pseudoRandomBytes(10).toString('hex'),
-        title: 'Title',
-        author: 'Author',
-        displayImage:
-          'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-        slug: 'testing',
+  const { method } = req;
+
+  if (method !== 'GET') return res.status(404).end();
+
+  try {
+    const session = await getServerAuthSession({ req, res });
+
+    if (!session || !session.user || !session.user.id)
+      return res.status(401).end();
+
+    const currentBooks = await prisma.reservation.findMany({
+      where: {
+        userId: session.user.id,
+        status: {
+          notIn: ['OVERDUE', 'RETURN'],
+        },
       },
-      {
-        id: crypto.pseudoRandomBytes(10).toString('hex'),
-        title: 'Title 2',
-        author: 'Author 3',
-        displayImage:
-          'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-        slug: 'testing',
+      include: {
+        book: {
+          include: {
+            author: true,
+          },
+        },
       },
-    ],
-    favoriteAuthor: [
-      {
-        name: 'Author 1',
-        slug: 'testing',
+    });
+
+    const mostVisitedGenres = await prisma.genreUserCount.findMany({
+      where: {
+        userId: session.user.id,
       },
-      {
-        name: 'Author 2',
-        slug: 'testing2',
+      orderBy: [
+        {
+          count: 'asc',
+        },
+      ],
+      include: {
+        genre: true,
       },
-    ],
-  });
+      take: 10,
+    });
+
+    res.status(200).json({
+      current: JSON.parse(JSON.stringify(currentBooks)),
+      favoriteGenres: mostVisitedGenres,
+      favoriteAuthor: [
+        {
+          name: 'Author 1',
+          slug: 'testing',
+        },
+        {
+          name: 'Author 2',
+          slug: 'testing2',
+        },
+      ],
+    });
+  } catch (err) {
+    res.status(500).end();
+  }
 }
