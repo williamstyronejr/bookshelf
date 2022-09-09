181809de709ba528016d776debe1836e729d668c
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-09 11:00:41 -0400

    Adds view modes

--- src/pages/library/search.tsx
+++ src/pages/library/search.tsx
@@ -3,9 +3,11 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import useInfiniteScroll from 'react-infinite-scroll-hook';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function SearchPage() {
   const { query } = useRouter();
+  const [viewMode, setViewMode] = useState('grid');
 
   const {
     data,
@@ -46,18 +48,52 @@ export default function SearchPage() {
   });
 
   return (
-    <section className="flex flex-col flex-nowrap h-full">
-      <header className="h-20 shrink-0"></header>
+    <section className="flex flex-col flex-nowrap">
+      <header className="h-20 shrink-0">
+        <button
+          className="disabled:text-gray-500 mr-4"
+          disabled={viewMode === 'grid'}
+          onClick={() => setViewMode('grid')}
+        >
+          <i className="fas fa-th-large text-2xl" />
+          <span className="block text-sm">Grid</span>
+        </button>
+
+        <button
+          className="disabled:text-gray-500"
+          type="button"
+          disabled={viewMode === 'list'}
+          onClick={() => setViewMode('list')}
+        >
+          <i className="fas fa-list text-2xl" />
+          <span className="block text-sm">List</span>
+        </button>
+      </header>
 
       <div className="flex-grow h-1 max-w-[800px]">
-        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(8rem,_1fr))] gap-4">
+        <ul
+          className={`${
+            viewMode === 'grid'
+              ? 'grid grid-cols-[repeat(auto-fit,_minmax(8rem,_1fr))] gap-4'
+              : 'flex flex-col flex-nowrap'
+          }`}
+        >
           {data &&
             data.pages.map((page) =>
               page.results.map((book) => (
-                <li key={book.id}>
-                  <Link href={`/book/${book.slug}`}>
+                <li
+                  key={book.id}
+                  className={`${
+                    viewMode === 'list' ? 'flex flex-row flex-nowrap my-2' : ''
+                  }`}
+                >
+                  <Link href={`/book/${book.id}/${book.slug}`}>
                     <a>
-                      <div className="relative w-32 h-40">
+                      <div
+                        className={`relative w-32 h-40 ${
+                          viewMode === 'list' ? 'mr-4' : ''
+                        }`}
+                      >
                         <Image
                           className="rounded-lg"
                           priority={true}
@@ -66,13 +102,18 @@ export default function SearchPage() {
                           alt="Book covers"
                         />
                       </div>
-                      <div className="font-medium">{book.title}</div>
                     </a>
                   </Link>
 
-                  <Link href={`/author/${book.author}`}>
-                    <a className="text-gray-600">{book.author}</a>
-                  </Link>
+                  <div>
+                    <Link href={`/book/${book.id}/${book.slug}`}>
+                      <div className="font-medium">{book.title}</div>
+                    </Link>
+
+                    <Link href={`/author/${book.author}`}>
+                      <a className="text-gray-600">{book.author.name}</a>
+                    </Link>
+                  </div>
                 </li>
               ))
             )}
