76a0b32d38b3f1ae2a42b7151f247639d6e1918d
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-27 03:06:43 -0400

    Updated styling

--- src/pages/user/settings/[type].tsx
+++ src/pages/user/settings/[type].tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import * as React from 'react';
+import { useState, SyntheticEvent } from 'react';
 import Link from 'next/link';
 import Input from '../../../components/Input';
 import { validateNewPassword, validateUser } from '../../../utils/validation';
 
 const PasswordForm = () => {
-  const [fieldErrors, setFieldErrors] = React.useState<{
+  const [fieldErrors, setFieldErrors] = useState<{
     oldPassword?: string;
     newPassword?: string;
     confirmPassword?: string;
   }>({});
-  const handleSubmit = (evt: React.SyntheticEvent<HTMLFormElement>) => {
+  const handleSubmit = (evt: SyntheticEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
     const formData = new FormData(evt.currentTarget);
@@ -60,12 +60,12 @@ const PasswordForm = () => {
 };
 
 const AccountForm = () => {
-  const [fieldErrors, setFieldErrors] = React.useState<{
+  const [fieldErrors, setFieldErrors] = useState<{
     username?: string;
     email?: string;
   }>({});
 
-  const handleSubmit = (evt: React.SyntheticEvent<HTMLFormElement>) => {
+  const handleSubmit = (evt: SyntheticEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
     const formData = new FormData(evt.currentTarget);
@@ -81,9 +81,6 @@ const AccountForm = () => {
 
   return (
     <form className="" onSubmit={handleSubmit}>
-      <header>
-        <h3>Account</h3>
-      </header>
       <fieldset className="">
         <Input
           name="username"
@@ -116,31 +113,46 @@ const SettingsPage: NextPage = () => {
   const { query } = useRouter();
 
   return (
-    <section className="">
+    <section className="max-w-2xl mx-auto">
       <header className="mb-4">
         <h4 className="font-medium">Settings</h4>
       </header>
 
-      <aside>
-        <ul>
-          <li>
-            <Link href="/user/settings/account">
-              <a>Account</a>
-            </Link>
-          </li>
+      <div className="flex flex-col flex-nowrap md:flex-row border-[1px]">
+        <aside className="w-full md:w-48 shrink-0 border-b-[1px] md:border-r-[1px] md:border-b-0">
+          <ul className="w-full text-center md:shrink-0">
+            <li
+              className={`inline-block md:block w-3/6 md:w-full 
+            md:border-l-2 ${
+              query.type === 'account'
+                ? 'md:border-black dark:md:border-white bg-custom-bg-off-light dark:bg-custom-bg-off-dark font-semibold'
+                : 'md:border-transparent md:hover:border-gray-200 hover:bg-custom-bg-off-light hover:dark:bg-custom-bg-off-dark'
+            }`}
+            >
+              <Link href="/user/settings/account">
+                <a className="block w-full py-3 md:py-4">Account</a>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="/user/settings/password">
-              <a>Password</a>
-            </Link>
-          </li>
-        </ul>
-      </aside>
+            <li
+              className={`inline-block md:block w-3/6 md:w-full 
+            md:border-l-2 ${
+              query.type === 'password'
+                ? 'md:border-black dark:md:border-white bg-custom-bg-off-light dark:bg-custom-bg-off-dark font-semibold'
+                : 'md:border-transparent md:hover:border-gray-200 hover:bg-custom-bg-off-light hover:dark:bg-custom-bg-off-dark'
+            }`}
+            >
+              <Link href="/user/settings/password">
+                <a className="block w-full py-3 md:py-4">Password</a>
+              </Link>
+            </li>
+          </ul>
+        </aside>
 
-      <div className="bg-custom-background max-w-xl mx-auto py-4 px-12">
-        {query.type === 'account' ? <AccountForm /> : null}
-
-        {query.type === 'password' ? <PasswordForm /> : null}
+        <div className="flex-grow mx-auto py-4 px-12">
+          {query.type === 'account' ? <AccountForm /> : null}
+          {query.type === 'password' ? <PasswordForm /> : null}
+        </div>
       </div>
     </section>
   );
