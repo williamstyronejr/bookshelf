f40e56ece05d034a1f728816894e24766b34a386
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-27 13:30:20 -0400

    Updated styling and layout

--- src/pages/book/[id]/[slug]/reserve.tsx
+++ src/pages/book/[id]/[slug]/reserve.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
@@ -6,9 +6,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Redis from 'ioredis';
 import dayjs from 'dayjs';
+import { signIn } from 'next-auth/react';
 import { prisma } from '../../../../utils/db';
 import { getServerAuthSession } from '../../../../utils/serverSession';
-import { signIn } from 'next-auth/react';
 
 const redisClient = new Redis(process.env.REDIS_URL?.toString() || '');
 
@@ -77,6 +77,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 export default function ReservationPage({ book, available }) {
   const { query, push, reload } = useRouter();
+  const [reserveLength, setReserveLength] = useState('7');
+  const [menu, setMenu] = useState(false);
+
   const {
     data: mutatedData,
     mutate,
@@ -112,20 +115,11 @@ export default function ReservationPage({ book, available }) {
     }
   );
 
-  function submitHandler(evt: React.SyntheticEvent<HTMLFormElement>) {
-    evt.preventDefault();
-
-    const formData = new FormData(evt.currentTarget);
-    const fields: any = Object.fromEntries(formData.entries());
-
-    mutate({ reserveLength: fields.time });
-  }
-
   if (!book) return <div>Loading...</div>;
   if (!available) return <NotAvailable />;
 
   return (
-    <section className="relative">
+    <section className="relative max-w-2xl md:mx-auto">
       <div
         className={`flex-col flex-nowrap w-full h-full absolute z-10 bg-custom-background items-center justify-center ${
           mutatedData ? 'flex' : 'hidden'
@@ -142,44 +136,81 @@ export default function ReservationPage({ book, available }) {
       </div>
 
       <header>
-        <h3 className="text-center">Reservation</h3>
+        <h3 className="">Reservation</h3>
       </header>
 
-      <div className="flex flex-row flex-nowrap px-4">
-        <aside className="mr-4">
-          <div className="relative w-32 h-40">
+      <div className="flex flex-col md:flex-row flex-nowrap px-4 text-center md:text-left">
+        <aside className="mr-4 mb-4 md:mb-0 flex-grow">
+          <div className="relative w-52 h-60 mx-auto md:mx-0">
             <Image
               className="rounded-lg"
               priority={true}
               layout="fill"
-              // objectFit="contain"
+              objectFit="contain"
               src={book.displayImage}
               alt="Book covers"
             />
           </div>
 
-          <div className="">{book.title}</div>
-          <div className="">{book.author.name}</div>
+          <div className="text-center md:text-left w-40 md:w-auto mx-auto md:mx-0 mt-4 md:mt-0">
+            {book.title}
+          </div>
+
+          <Link href={`/author/${book.author.id}/${book.author.slug}`}>
+            <a className="text-custom-text-light-subtle dark:text-custom-text-dark-subtle hover:text-custom-text-light hover:dark:text-custom-text-dark">
+              {book.author.name}
+            </a>
+          </Link>
         </aside>
 
-        <form className="" onSubmit={submitHandler}>
-          <fieldset>
-            <label>
-              <span className="block">Reservation Length</span>
+        <div className="text-center w-full md:w-auto md:text-left shrink-0">
+          <input
+            id="length"
+            name="length"
+            type="hidden"
+            value={reserveLength}
+            onChange={() => {}}
+          />
 
-              <select id="time" name="time">
-                <option value={7}>7 Days</option>
-                <option value={10}>10 Days</option>
-                <option value={20}>20 Days</option>
-                <option value={30}>30 Days</option>
-              </select>
-            </label>
-          </fieldset>
+          <button
+            className="w-full px-4 py-2 rounded-md mb-2 mx-auto bg-custom-bg-off-light dark:bg-custom-bg-off-dark"
+            aria-label="menu"
+            type="button"
+            onClick={() => setMenu((old) => !old)}
+          >
+            {reserveLength} Days
+            <i
+              className={`fas fa-arrow-down ml-4 transition ${
+                menu ? 'rotate-180' : 'rotate-0'
+              } `}
+            />
+          </button>
 
-          <button className="" disabled={isMutating}>
+          <div
+            className={`${
+              menu ? 'block' : 'hidden'
+            } bg-custom-bg-off-light dark:bg-custom-bg-off-dark px-1 py-1 mb-2 rounded-md`}
+          >
+            <button
+              className="w-full py-1"
+              type="button"
+              onClick={() => {
+                setReserveLength('7');
+                setMenu(false);
+              }}
+            >
+              7 Days
+            </button>
+          </div>
+
+          <button
+            className="text-white bg-custom-btn-submit py-2 px-4 mt-4 rounded-md"
+            onClick={() => mutate({ reserveLength })}
+            disabled={isMutating}
+          >
             Create Reservation
           </button>
-        </form>
+        </div>
       </div>
     </section>
   );
