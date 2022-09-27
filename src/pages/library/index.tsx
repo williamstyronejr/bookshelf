72dffe19afac78e7d4271019fc8629b2e5ce0e35
williamstyronejr <tyronewilliamsjr@hotmail.com>
2022-09-27 03:08:25 -0400

    Add carousels

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { prisma } from '../../utils/db';
+import Carousel from '../../components/Carousel';
 
 export const getServerSideProps = async () => {
   let mostRecentBooks = await prisma.book.findMany({
@@ -34,29 +35,33 @@ export const getServerSideProps = async () => {
     },
   });
 
-  const trendingGenres = await prisma.genre.findMany({
+  const trendingGenres = await prisma.bookGenres.findMany({
     take: 10,
     select: {
-      id: true,
-      name: true,
+      genre: true,
+      book: {
+        select: {
+          id: true,
+          slug: true,
+          displayImage: true,
+        },
+      },
     },
+    distinct: ['genreId'],
   });
 
   // Hacky way of dealing with Date Object not having a toJSON
-  mostRecentBooks = JSON.parse(JSON.stringify(mostRecentBooks));
-  topBooks = JSON.parse(JSON.stringify(topBooks));
-
   return {
     props: {
-      mostRecentBooks,
-      topBooks,
+      mostRecentBooks: JSON.parse(JSON.stringify(mostRecentBooks)),
+      topBooks: JSON.parse(JSON.stringify(topBooks)),
       trendingGenres: JSON.parse(JSON.stringify(trendingGenres)),
     },
   };
 };
 
 const BookItem = ({ book }) => (
-  <li className="relative flex-grow w-1/5 mr-6 max-w-[175px]">
+  <div className="relative shrink-0 w-32 mr-6 max-w-[175px]">
     <Link href={`/book/${book.id}/${book.slug}`}>
       <a>
         <div className="relative h-56 shadow-md mb-2">
@@ -68,7 +73,9 @@ const BookItem = ({ book }) => (
             alt="Book covers"
           />
         </div>
-        <div className="font-medium">{book.title}</div>
+        <div className="font-medium whitespace-nowrap text-ellipsis overflow-hidden">
+          {book.title}
+        </div>
       </a>
     </Link>
 
@@ -77,7 +84,7 @@ const BookItem = ({ book }) => (
         <div className="text-gray-600">{book.author.name}</div>
       </a>
     </Link>
-  </li>
+  </div>
 );
 
 export default function LibraryPage({
@@ -86,27 +93,27 @@ export default function LibraryPage({
   trendingGenres,
 }) {
   return (
-    <section>
+    <section className="max-w-7xl mx-auto">
       <header className="">
         <h4 className="my-6 text-xl font-medium text-center">Most Popular</h4>
-        <ul className="flex flex-row flex-nowrap w-full">
+        <Carousel>
           {topBooks
             ? topBooks.map((book) => (
-                <BookItem key={`popular-${book.id}`} book={book} />
+                <BookItem key={`popular-book-${book.id}`} book={book} />
               ))
             : null}
-        </ul>
+        </Carousel>
       </header>
 
       <div className="block">
         <h4 className="my-6 text-xl font-medium text-center">New Releases</h4>
-        <ul>
+        <Carousel>
           {mostRecentBooks
             ? mostRecentBooks.map((book) => (
-                <BookItem key={`recent-${book.id}`} book={book} />
+                <BookItem key={`recent-book-${book.id}`} book={book} />
               ))
             : null}
-        </ul>
+        </Carousel>
       </div>
 
       <div className="">
@@ -114,14 +121,34 @@ export default function LibraryPage({
           Trending Genres
         </h4>
 
-        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(8rem,_1fr))] gap-4 divide">
+        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(min(100%/3,_max(8rem,_100%/5)),_1fr))] gap-4 max-w-2xl mx-auto divide">
           {trendingGenres
-            ? trendingGenres.map((genre) => (
+            ? trendingGenres.map((data) => (
                 <li
-                  key={`trending-genre-${genre.id}`}
-                  className="text-center py-4  rounded-lg"
+                  key={`trending-genre-${data.genre.id}`}
+                  className="w-32 text-center py-4"
                 >
-                  {genre.name}
+                  <div className="relative w-32 h-52 mx-auto mb-4 border-2 border-transparent hover:border-black rounded-lg">
+                    <Link href={`/book/${data.book.id}/${data.book.slug}`}>
+                      <a>
+                        <Image
+                          className="rounded-lg"
+                          priority={true}
+                          layout="fill"
+                          src={data.book.displayImage}
+                          alt="Book cover"
+                        />
+                      </a>
+                    </Link>
+                  </div>
+
+                  <div>
+                    <Link href={`/search?genre=${data.genre.id}`}>
+                      <a className="hover:underline hover:text-custom-text-highlight-light dark:hover:text-custom-text-highlight-dark">
+                        {data.genre.name}
+                      </a>
+                    </Link>
+                  </div>
                 </li>
               ))
             : null}
